7324720ed344666fc10a1b1c195e556807d47326
Sachin Srivastava <sachinrising@gmail.com>
2015-12-04 04:50:25 +0530

    Added Test cases & added Store Controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, :price, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
-	validates :title, uniqueness: true
+	validates :title, uniqueness: {unique: true, message: "Bhai title pehle se h chori h"}, length: {minimum: 10, message: '%{value} Bhai check kar rha bura na manna'}
+	# validates :title, length: {minimum: 10, message: '%{value} Bhai check kar rha bura na manna'}
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
