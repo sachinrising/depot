5cf5f277352d66cb4c31eac29a7142b2a94c424b
Sachin Srivastava <sachinrising@gmail.com>
2015-12-06 22:39:32 +0530

    Changes made

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+	def getTime
+		@time = Time.now
+
+		hour = @time.hour
+
+		minute = @time.strftime("%M")
+
+		second = @time.strftime("%S")
+
+		hFormat = "AM"
+
+		hFormat = "PM" if hour >= 12
+
+		hour = hour - 12 if hour > 12
+
+		"#{hour}:#{minute}:#{second} #{hFormat}"
+	end
 end
